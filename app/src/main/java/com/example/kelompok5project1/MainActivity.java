5823f8140e8136b1a311c7362ed4bc6e397e2ba5
erhansayuferdiansya <erhansayu@gmail.com>
2023-05-11 16:45:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kelompok5project1/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.kelompok5project1;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private TaskDatabaseHelper taskHelper;
+    private ListView TaskList;
+    private ArrayAdapter<String> arrAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        taskHelper = new TaskDatabaseHelper(this);
+        TaskList = findViewById(R.id.list_todo);
+
+        updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.list_todo) {
+            final EditText taskEdit = new EditText(this);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Add a new task").setMessage("What do you want to do next?").setView(taskEdit)
+                    .setPositiveButton("Add", (dialog1, which) -> {
+                        String task = String.valueOf(taskEdit.getText());
+                        SQLiteDatabase db = taskHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                        db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                        db.close();
+                        updateUI();
+                    })
+                    .setNegativeButton("Cancel", null).create();
+            dialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = taskHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (arrAdapter == null) {
+            arrAdapter = new ArrayAdapter<>(this, R.layout.activity_main, R.id.list_todo, taskList);
+            TaskList.setAdapter(arrAdapter);
+        } else {
+            arrAdapter.clear();
+            arrAdapter.addAll(taskList);
+            arrAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+    }
+}
